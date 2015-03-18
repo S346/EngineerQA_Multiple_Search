be5cd6157b2c67a972b9cdcd9894626361d296b4
Sanshiro Sakai <xosya3@gmail.com>
2015-03-19 04:02:34 +0900

    QA@IT検索追加、検索チェックボックス追加

--- Search.py
+++ Search.py
@@ -12,6 +12,20 @@ from bottle import TEMPLATE_PATH, jinja2_template as template
 TEMPLATE_PATH.append("./views")
 
 
+# スクレイピング用（検索用URL, 質問表示用URL, 検索タグ, 検索値）
+def scraping(s_url, q_url, tag, value):
+    titles = []
+    urls = []
+    r = urllib2.urlopen(s_url)
+    soup = BeautifulSoup(r.read())
+    s = soup.find_all(tag, {"class": value})
+    for x in s:
+        titles.append(x.a['title'])
+        urls.append(q_url + x.a['href'])
+    r.close()
+    return urls, titles
+
+
 # Qiita検索
 def qiita_search(word):
     qiita_api = 'http://qiita.com/api/v2/items?page=1&per_page=10&&query=' + \
@@ -29,7 +43,7 @@ def qiita_search(word):
     return urls, titles
 
 
-# Stack Over Flow検索用関数
+# Stack Over Flow検索
 def sof_search(word):
     sof_api = 'https://api.stackexchange.com/2.2/search/advanced?pagesize=10&order=desc&sort=activity&title=' + \
         word + '&site=ja.stackoverflow'
@@ -48,34 +62,37 @@ def sof_search(word):
     return urls, titles
 
 
-# teratail検索用
+# teratail検索
 def tera_search(word):
-    tera_url = 'https://teratail.com/questions/search?q=' + \
+    s_url = 'https://teratail.com/questions/search?q=' + \
         word + '&search_type=and'
-    titles = []
-    urls = []
-    r = urllib2.urlopen(tera_url)
-    soup = BeautifulSoup(r.read())
-    s = soup.find_all("h2", {"class": "ttlItem"})
-    for lis in s:
-        titles.append(lis.string)
-        urls.append(lis.a['herf'])
-    r.close()
-    return titles, urls
+    q_url = 'https://teratail.com'
+    return scraping(s_url, q_url, 'h2', 'ttlItem')
+
+
+# QA@IT検索用
+def qait_search(word):
+    s_url = 'http://qa.atmarkit.co.jp/q/search?utf8=%E2%9C%93&q=' + \
+        word + '&commit=%E6%A4%9C%E7%B4%A2'
+    q_url = 'http://qa.atmarkit.co.jp'
+    return scraping(s_url, q_url, 'article', 'question')
 
 
 @route('/')
-def Top(name='it'):
-    return template('hello.j2', name=name)
+def Top():
+    return template('hello.j2')
 
 
 @get('/search')
 def search():
     # formの値を取得→utf-8に変換→クエリ用の値に変換
     word = urllib.quote_plus(request.query.search_word.encode('utf-8'))
+    ch = request.params.getlist('need')
     # urls, titles = qiita_search(word)
-    urls, titles = sof_search(word)
+    # urls, titles = sof_search(word)
+    # urls, titles = tera_search(word)
+    urls, titles = qait_search(word)
     word_utf8 = urllib.unquote_plus(word).decode('utf-8')
-    return template('result.j2', a=word_utf8, titles=titles, urls=urls)
+    return template('result.j2', a=word_utf8, titles=titles, urls=urls, b=ch)
 
 run(host='localhost', port=8080, debug=True, reloader=True)
